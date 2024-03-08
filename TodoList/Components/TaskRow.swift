dfac09bc8e68b629b4ce4c8b46a986fbf8999d76
KimWooSung <kipsong133@gmail.com>
2024-03-08 12:03:34 +0900

    Feat: 투두리스트 Row 레이아웃 구성

---
+++ TodoList/Components/TaskRow.swift
@@ -0,0 +1,27 @@
+//
+//  TaskRow.swift
+//  TodoList
+//
+//  Created by 김우성 on 2024/03/08.
+//
+
+import SwiftUI
+
+struct TaskRow: View {
+    // View에 필요한 값들
+    var task: String
+    var completed: Bool
+    
+    var body: some View {
+        HStack {
+            Image(systemName: completed ? "checkmark.circle.fill" : "circle")
+            Text(task)
+        }
+    }
+}
+
+struct TaskRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskRow(task: "면도 개멋잇게 하기", completed: true)
+    }
+}
